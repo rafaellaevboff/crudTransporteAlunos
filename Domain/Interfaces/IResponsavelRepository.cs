67118af0255a72c92f5ed63deb7727b79b1894f9
bryanferreira23 <bryanferreira2301@rede.ulbra.br>
2022-07-01 19:29:39 -0300

    primeira parte

---
+++ Domain/Interfaces/IResponsavelRepository.cs
@@ -0,0 +1,9 @@
+using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IResponsavelRepository : IBaseRepository<Responsavel>
+    {
+         
+    }
+}
--- Domain/Interfaces/IUnitOfWork.cs
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -5,6 +5,7 @@ namespace Domain.Interfaces
          Task CommitAsync();
 
         //add as interfaces que usarão o UnitOfWork
-         IUserRepository UserRepository{get;}
+         IServicoRepository ServicoRepository{get;}
+         IResponsavelRepository ResponsavelRepository{get;}
     }
 }
